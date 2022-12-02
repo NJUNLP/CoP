6c5ad45e45b0432a0db2606c79b5eb45a1650a55
kevinpro <43642508+Ricardokevins@users.noreply.github.com>
2022-12-03 00:32:51 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,1 +1,1 @@
-python3 testarg.py --data_path 'toy.json' --mode 'zero-shot' --model_path '../../../Data/PLM/BARTCNN' --output_file_path "output/result.json"
+python3 inference.py --data_path 'data/toy.json' --mode 'zero-shot' --model_path '../../../Data/PLM/BARTCNN' --output_file_path "output/result.json" --predict_raio 0.6
